d192457d44726fd020fce03e8e90d1e3861fe0c2
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-07-16 11:23:44 +0300

    generic return type

--- index.ts
+++ index.ts
@@ -6,3 +6,13 @@ const last = <T>(arr: T[]): T => {
 const l = last([1, 3, 5]);
 
 const l2 = last(['a', 'b', 'c']);
+
+// Another example.
+
+const createArray = <T>(value: T) => {
+  return [value];
+};
+
+const v = createArray(2);
+
+const v2 = createArray('Y');
