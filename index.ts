36e1729b0a0f461e5fe8e03ee6d26981966e9215
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-07-16 11:29:09 +0300

    Override the generic value

--- index.ts
+++ index.ts
@@ -9,10 +9,14 @@ const l2 = last(['a', 'b', 'c']);
 
 // Another example.
 
-const createArray = <T>(value: T) => {
-  return [value];
+const createArray = <T, D>(firstValue: T, secondValue: D): [T, D] => {
+  return [firstValue, secondValue];
 };
 
-const v = createArray(2);
+const v = createArray(2, 3);
 
-const v2 = createArray('Y');
+const v2 = createArray('Y', 'X');
+
+const v3 = createArray('Y', 2);
+// Override tyepes
+const v4 = createArray<string | null, number>(null, 2);
